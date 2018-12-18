badec7100fafcedafb87874fba0607da2f90e016
qzs <qzsiniong@163.com>
2018-12-18 16:32:49 +0800

    1.actionTypes修改为class 2.支持【右键菜单】和【command】

--- README.md
+++ README.md
@@ -1,16 +1,46 @@
 # redux-action-types README
 
-redux action types 自动化工具
+redux action types 自动化工具1
 
 ## Features
 在编写redux action types 时，自动生成值,在js或ts文件中使用`// enableAutoFixActionTypes`来启用功能
 ```js
 // enableAutoFixActionTypes
 
-const AUTH_LOGIN = "";
+// AUTH_LOGIN->登录
+// SPIDERS_SET_TASK_ID=
+export default class ActionTypes {
+}
+
 //   |
 //   ∨
-const AUTH_LOGIN = "auth_login";
+
+// enableAutoFixActionTypes
+
+// AUTH_LOGIN->登录
+// SPIDERS_SET_TASK_ID=
+export default class ActionTypes {
+
+    /**
+     * 登录-开始
+     */
+    static readonly AUTH_LOGIN_REQUEST = "auth_login_request"
+    /**
+     * 登录-成功
+     */
+    static readonly AUTH_LOGIN_SUCCESS = "auth_login_success"
+    /**
+     * 登录-失败
+     */
+    static readonly AUTH_LOGIN_FAILURE = "auth_login_failure"
+
+    // -------------------------
+
+    /**
+     * ---
+     */
+    static readonly SPIDERS_SET_TASK_ID = "spiders_set_task_id"
+}
 ```
 
 ## Requirements
--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,21 @@
 'use strict';
-import { window, workspace, ExtensionContext, TextDocumentWillSaveEvent, TextEdit, TextDocumentSaveReason, Range, Position } from 'vscode';
+import { window, workspace, ExtensionContext, TextDocumentWillSaveEvent, TextEdit, TextDocumentSaveReason, Range, Position, TextLine, commands } from 'vscode';
+import * as vscode from 'vscode';
 
 
 export function activate(context: ExtensionContext) {
 
     const disposable = workspace.onWillSaveTextDocument(willSaveTextDocument);
-
     context.subscriptions.push(disposable);
+
+    context.subscriptions.push(commands.registerCommand('extension.auto-fix-redux-action-types', (e) => {
+        let editor = window.activeTextEditor;
+        if (!editor) {
+            return;
+        }
+
+        doFixAllProblems(editor);
+    }));
 }
 
 function willSaveTextDocument(e: TextDocumentWillSaveEvent) {
@@ -14,26 +23,80 @@ function willSaveTextDocument(e: TextDocumentWillSaveEvent) {
     const languageIds = ['typescript', 'javascript'];
 
     const documentText = document.getText();
-    const isEnableAutoFixActionTypes = documentText.includes('// enableAutoFixActionTypes');
-
+    const isEnableAutoFixActionTypes = /^\/\/\s*enableAutoFixActionTypes/.test(documentText);
     if (languageIds.indexOf(document.languageId) > -1 && isEnableAutoFixActionTypes && e.reason === TextDocumentSaveReason.Manual) {
-        doFixAllProblems(e);
+        doFixAllProblems(undefined, e);
     }
 }
 
-function doFixAllProblems(e: TextDocumentWillSaveEvent) {
-    const document = e.document;
+
+function doFixAllProblems(editor?: vscode.TextEditor, e?: TextDocumentWillSaveEvent) {
+    const document = editor ? editor.document : (e ? e.document : null);
+    if (!document) {
+        return;
+    }
     const lineCount = document.lineCount;
-    let text = document.getText();
+    const regClsStart = /class\s+\w+\s*{/;
+    const regClsEnd = /^}$/;
+    const reg = /\s*\/\/\s*(\w+)\s*->\s*(.*)/;
+    const reg1 = /\s*\/\/\s*(\w+)\s*=\s*(.*)/;
+    const generats: Array<string> = [];
+    let lineNumber: number,
+        line: TextLine,
+        clsStartLine = 0,
+        clsEndLine = 0;
 
-    text = text.replace(/(const\s+)(\w+)(\s*=\s*")([^"]*)(")/g, (_, ...args) => {
-        const arr:string[] = args.slice(0,-2);
-        arr[3] = arr[1].toLowerCase();
-        return arr.join('');
-    });
+    for (lineNumber = 0; lineNumber < lineCount; lineNumber++) {
+        line = document.lineAt(lineNumber);
+        if (regClsStart.test(line.text)) {
+            clsStartLine = lineNumber;
+            break;
+        }
+    }
 
-    const replaceEdit = TextEdit.replace(new Range(new Position(0, 0), new Position(lineCount,0)), text);
+    for (lineNumber = lineCount - 1; lineNumber >= 0; lineNumber--) {
+        line = document.lineAt(lineNumber);
+        if (regClsEnd.test(line.text)) {
+            clsEndLine = lineNumber;
+            break;
+        }
+    }
 
-    e.waitUntil(Promise.resolve([replaceEdit]));
-    window.showInformationMessage("auto fix action types success");
+    for (lineNumber = 0; lineNumber < lineCount; lineNumber++) {
+        line = document.lineAt(lineNumber);
+        const match = line.text.match(reg);
+        if (match) {
+            const name = match[1];
+            const comment = match[2] || '---';
+
+            generats.push([
+                `\n    /**\n     * ${comment}-开始\n     */\n    static readonly ${name.toLocaleUpperCase()}_REQUEST = "${name.toLocaleLowerCase() + '_request'}"`,
+                `\n    /**\n     * ${comment}-成功\n     */\n    static readonly ${name.toLocaleUpperCase()}_SUCCESS = "${name.toLocaleLowerCase() + '_success'}"`,
+                `\n    /**\n     * ${comment}-失败\n     */\n    static readonly ${name.toLocaleUpperCase()}_FAILURE = "${name.toLocaleLowerCase() + '_failure'}"\n`,
+            ].join(''));
+        }
+
+        const match1 = line.text.match(reg1);
+        if (match1) {
+            const name = match1[1];
+            const comment = match1[2] || '---';
+
+            generats.push([
+                `\n    /**\n     * ${comment}\n     */\n    static readonly ${name.toLocaleUpperCase()} = "${name.toLocaleLowerCase()}"\n`,
+            ].join(''));
+        }
+    }
+
+    if (editor) {
+        editor.edit((editBuilder: vscode.TextEditorEdit) => {
+            editBuilder.replace(new Range(new Position(clsStartLine + 1, 0), new Position(clsEndLine, 0)), generats.join('\n    // ------------------------------------------------------------------------------------\n'));
+            window.showInformationMessage("auto fix action types success");
+        });
+    }
+
+    if (e) {
+        const replaceEdit = TextEdit.replace(new Range(new Position(clsStartLine + 1, 0), new Position(clsEndLine, 0)), generats.join('\n    // ------------------------------------------------------------------------------------\n'));
+        e.waitUntil(Promise.resolve([replaceEdit]));
+        window.showInformationMessage("auto fix action types success");
+    }
 }
